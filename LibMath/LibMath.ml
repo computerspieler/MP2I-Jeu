9862c4abc395d17a1dc939aabc1ddade1ba8fb8b
ComputerSpieler <36859459+computerspieler@users.noreply.github.com>
2022-02-17 00:36:20 +0100

    LibMath: Ajout de clamp et ceil_div
    Ces deux fonctions permettent de faire un encadrement
    et calculer la partie entiere superieur d'une division.

--- LibMath/LibMath.ml
+++ LibMath/LibMath.ml
@@ -1,2 +1,15 @@
+module Vec2 = struct
+	include Vec2
+end
+
 let to_radian deg =
 	deg *. Float.pi /. 180.
+
+let ceil_div a b =
+	(a + b - 1) / b
+
+let clamp i min max =
+	assert (min < max);
+	     if i < min then min
+	else if i > max then max
+	else i
--- LibMath/LibMath.mli
+++ LibMath/LibMath.mli
@@ -1,1 +1,44 @@
+module Vec2: sig
+	type vec2 = {
+		mutable x: int;
+		mutable y: int;
+	}
+
+	(* Operations avec un vecteur *)
+	(* Cree un vecteur *)
+	val create: int -> int -> vec2
+	(* Affiche le contenu d'un vecteur *)
+	val print: vec2 -> unit
+	(* Calcul la longueur d'un vecteur *)
+	val length: vec2 -> int
+	(* Renvoie le vecteur normalise
+	Renvoie un vecteur nul si la norme du vecteur est nul *)
+	val normalize: vec2 -> vec2
+
+	(* operations entre vecteurs *)
+	(* Renvoie la somme de 2 vecteurs *)
+	val add: vec2 -> vec2 -> vec2
+	(* Renvoie la difference de 2 vecteurs *)
+	val sub: vec2 -> vec2 -> vec2
+	(* Renvoie le produit de 2 vecteurs *)
+	val mul: vec2 -> vec2 -> vec2
+	(* Renvoie l de 2 vecteurs *)
+	val div: vec2 -> vec2 -> vec2
+
+	(* operations entre vecteur et constante *)
+	(* Renvoie la somme d'1 vecteur et d'une constante *)
+	val add_c: vec2 -> int -> vec2
+	(* Renvoie la difference d'1 vecteur et d'une constante *)
+	val sub_c: vec2 -> int -> vec2
+	(* Renvoie le produit d'1 vecteur et d'une constante *)
+	val mul_c: vec2 -> int -> vec2
+	(* Renvoie le produit d'1 vecteur et d'une constante *)
+	val div_c: vec2 -> int -> vec2
+
+	(* Renvoie le produit scalaire de 2 vecteurs *)
+	val scalaire: vec2 -> vec2 -> int
+end
+
 val to_radian : float -> float
+val ceil_div : int -> int -> int
+val clamp : int -> int -> int -> int
